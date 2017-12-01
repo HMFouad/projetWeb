dcabf0495fcddbffd03d8ece8179545843d05f60
caseaujeanne <jeanne.caseau@hotmail.fr>
2017-12-01 15:15:51 +0100

    debut de la connexion

--- hbcc-app/server/api/api.js
+++ hbcc-app/server/api/api.js
@@ -34,4 +34,18 @@ router.get('/users', (req, res) => {
     });
 });*/
 
+router.post('/tokens',(req,res)=>{
+    var user=new User();
+    user.email=req.body.email;
+    user.password=req.body.password;
+    if(user.email==null || user.password==null){
+        res.status(422).json({success:false,message:"Missing email or password"});
+    }else{
+        User.findOne({email:user.email,password:user.password},function(err,user){
+            var token= new Access_token();
+            
+        });
+    }
+});
+
 module.exports = router;
