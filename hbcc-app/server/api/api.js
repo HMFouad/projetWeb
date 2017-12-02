3783351abae1da27969edbe2ae1270f7314fc267
qcharria <quentin_charriau@orange.fr>
2017-12-02 15:20:11 +0100

    Recuperation des evenements etudiant d'un fichier ICS local et affichage des events correspondant aux jour de la semaine

--- hbcc-app/server/api/api.js
+++ hbcc-app/server/api/api.js
@@ -37,21 +37,23 @@ router.get('/events', function(req, res, next) {
     var date=[];
     //Récupération de la description et des dates
     for (i = 0, len = cours.length; i < len-1; i++) {
-      //console.log(cours[i]);
       var splited= cours[i].split('\\n');
       var descr=(splited[0]+splited[2]+splited[3]).split("\n");
       description.push(descr[0]);
       date.push(splited[1]);
     }
     //Découpage des dates pour récupérer date de début et date de fin
+    //On split jusqua pouvoir remplir les tableaux avec des string de la forme "year,month,day,hour,minute,second"
     var beginDate=[];
     var endDate=[];
     for (i = 0, len = date.length; i < len; i++) {
       var splited= date[i].split(" ");
-      beginDate.push({jour:splited[3],heure:splited[5]});
-      endDate.push({jour:splited[3],heure:splited[7]});
+      var splitDate=splited[3].split("/");
+      var beginTime=splited[5].split(":");
+      var endTime=splited[7].split(":");
+      beginDate.push(splitDate[2]+","+splitDate[1]+","+splitDate[0]+","+beginTime[0]+","+beginTime[1]);
+      endDate.push(splitDate[2]+","+splitDate[1]+","+splitDate[0]+","+endTime[0]+","+endTime[1]);
     }
-
 
     //Récupération de la localisation
     var blocks= data.split("SEQUENCE");
@@ -60,10 +62,10 @@ router.get('/events', function(req, res, next) {
       var splited= blocks[i].split("LOCATION:");
       location.push(splited[1]);
     }
-    console.log("Description : "+description[0]);
-    console.log("Date de début : "+beginDate[0].jour + " " + beginDate[0].heure);
-    console.log("Date de fin : "+endDate[0].jour + " " + endDate[0].heure);
-    console.log("Localisation : "+location[0]);
+    /*console.log("Description : "+description[0]);
+    console.log("Date de début : "+beginDate[0]);
+    console.log("Date de fin : "+endDate[0]);
+    console.log("Localisation : "+location[0]);*/
 
     var events = {description : description,
                   beginDate : beginDate,
--- hbcc-app/src/app/events/events.component.ts
+++ hbcc-app/src/app/events/events.component.ts
@@ -17,20 +17,33 @@ export class EventsComponent implements OnInit {
   jeudi : {day:string,events:any[]};
   vendredi : {day:string,events:any[]};
 
-  data:any = {};
+  data:any;
+  description:string[];
+  beginDate:string[];
+  endDate:string[];
+  location:string[];
 
   constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-
    this.getData();
   }
  getData() {
     this.http.get('/api/events').subscribe(data => {
-      this.data = data;
-       console.log("Donnes");
-       console.log(data.description);
-       this.getWeekEvents();
+    this.data=data;
+    this.description = this.data.description;
+    this.beginDate = this.data.beginDate;
+    this.endDate = this.data.endDate;
+    this.location = this.data.location;
+    console.log("Descriptions des cours");
+    console.log(this.description);
+    console.log("Date de début des cours");
+    console.log(this.beginDate);
+    console.log("Date de fin des cours");
+    console.log(this.endDate);
+    console.log("Localisation des cours");
+    console.log(this.location);
+    this.getWeekEvents();
 
     });
  }
@@ -42,29 +55,42 @@ export class EventsComponent implements OnInit {
    this.vendredi=  {day:"01/12/2017", events:new Array()};
 
 
-   console.log(this.data.description);
-   console.log(this.data.description.length);
-   for (var i = 0; i < this.data.beginDate.length; i++) {
+  // console.log(this.data.description);
+  // console.log(this.data.description.length);
+   for (var i = 0; i < this.description.length; i++) {
     //On crée un nouvel évènement
-    var newEvent ={  description:this.data.description[i],
-                beginDate:new Date(this.data.beginDate[i].jour),
-                endDate:new Date(this.data.endDate[i].jour),
-                location: this.data.location[i] };
+    //console.log("On crée un évènement");
+    var beginDateSplited=this.beginDate[i].split(",");
+    var endDateSplited=this.endDate[i].split(",");
+    var newEvent ={   description:this.description[i],
+                      beginDate:new Date(beginDateSplited[0]+"-"+beginDateSplited[1]+"-"+beginDateSplited[2]+"T"+beginDateSplited[3]+":"+beginDateSplited[4]+":00Z"),
+                      endDate:  new Date(endDateSplited[0]+"-"+endDateSplited[1]+"-"+endDateSplited[2]+"T"+endDateSplited[3]+":"+endDateSplited[4]+":00Z"),
+                      location: this.location[i] };
+    //console.log(newEvent);
      //On l'ajout dans la liste des évènements de la journée de la semaine qui lui correspond
-     switch(this.data.beginDate.jour) {
+     console.log("On l'ajoute dans la liste des évènements du jour de la smeaine qui lui correspond s'il y en a un.");
+     console.log(beginDateSplited[2]+"/"+beginDateSplited[1]+"/"+beginDateSplited[0]);
+     console.log(new Date(beginDateSplited[0]+"-"+beginDateSplited[1]+"-"+beginDateSplited[2]+"T"+beginDateSplited[3]+":"+beginDateSplited[4]+":00Z"));
+     var comp =beginDateSplited[2]+"/"+beginDateSplited[1]+"/"+beginDateSplited[0]
+     switch(comp) {
         case this.lundi.day:
+            console.log(this.lundi.day+"  -> Correspondance");
             this.lundi.events.push(newEvent);
             break;
         case this.mardi.day:
+            console.log(this.mardi.day+"  -> Correspondance");
             this.mardi.events.push(newEvent);
             break;
         case this.mercredi.day:
+            console.log(this.mercredi.day+"  -> Correspondance");
             this.mercredi.events.push(newEvent);
             break;
         case this.jeudi.day:
+            console.log(this.jeudi.day+"  -> Correspondance");
             this.jeudi.events.push(newEvent);
             break;
         case this.vendredi.day:
+            console.log(this.vendredi.day+"  -> Correspondance");
             this.vendredi.events.push(newEvent);
             break;
         default:
