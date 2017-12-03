0440566e2f034a7c8cf6556432b384cdef751794
Adrien Bounader <adrien.bounader@etu.u-bordeaux.fr>
2017-12-03 10:55:56 +0100

    refonte service GET events -> renvoie un tableau d'évènements

--- hbcc-app/server/api/services/events.js
+++ hbcc-app/server/api/services/events.js
@@ -1,64 +1,53 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const ObjectID = require('mongodb').ObjectID;
-const User = require('../mongoose/model/user.model');
-const connection = require ('../mongoose/connection');
 const fs = require('fs');
+const icalParser = require('ical');
+const Event = require('../mongoose/model/event.model');
+
+// get static file
+const FILE_PATH = "../Master2_4TGL901S_iCalendar.ics";
+
 
 // Get student events
-router.get('/', function(req, res, next) {
-    //On récupère le fichier ICS (ici local) et on le lit
-    var filePath="../Master2_4TGL901S_iCalendar.ics";
-    fs.readFile(filePath, {encoding: "utf8"}, function(err, data){
+router.get('/', (req, res, next) => {
+    console.log ('Api service call: GET events');
+    const ICS_TYPE_WANTED = 'VEVENT';
+
+    fs.readFile(FILE_PATH, {encoding: "utf8"}, (err, data) => {
         if (err) return next(err);
-        //On découpe le fichier pour récupérer les valeurs qui nous interessent
-        var blocks= data.split("DTEND");
-        var cours= [];
-        for (i = 0, len = blocks.length; i < len; i++) {
-            var splited= blocks[i].split("DESCRIPTION:");
-            cours.push(splited[1]);
-        }
-        var description=[];
-        var date=[];
-        //Récupération de la description et des dates
-        for (i = 0, len = cours.length; i < len-1; i++) {
-            var splited= cours[i].split('\\n');
-            var descr=(splited[0]+splited[2]+splited[3]).split("\n");
-            description.push(descr[0]);
-            date.push(splited[1]);
-        }
-        //Découpage des dates pour récupérer date de début et date de fin
-        //On split jusqua pouvoir remplir les tableaux avec des string de la forme "year,month,day,hour,minute,second"
-        var beginDate=[];
-        var endDate=[];
-        for (i = 0, len = date.length; i < len; i++) {
-            var splited= date[i].split(" ");
-            var splitDate=splited[3].split("/");
-            var beginTime=splited[5].split(":");
-            var endTime=splited[7].split(":");
-            beginDate.push(splitDate[2]+","+splitDate[1]+","+splitDate[0]+","+beginTime[0]+","+beginTime[1]);
-            endDate.push(splitDate[2]+","+splitDate[1]+","+splitDate[0]+","+endTime[0]+","+endTime[1]);
+
+        const events = [];
+
+        const parsedEvents = icalParser.parseICS(data);
+
+        for (const indexEvent in parsedEvents) {
+            if (parsedEvents.hasOwnProperty(indexEvent) &&
+                parsedEvents[indexEvent].type === ICS_TYPE_WANTED) {
+                const currentEvent = new Event();
+
+                // TODO parse description ?
+                if (parsedEvents[indexEvent].description) {
+                    currentEvent.title = parsedEvents[indexEvent].description;
+                }
+
+                if (parsedEvents[indexEvent].start) {
+                    currentEvent.beginDate = new Date(parsedEvents[indexEvent].start);
+                }
+
+                if (parsedEvents[indexEvent].end) {
+                    currentEvent.endDate = new Date (parsedEvents[indexEvent].end);
+                }
+
+                if (parsedEvents[indexEvent].location) {
+                    currentEvent.location = parsedEvents[indexEvent].location;
+                }
+
+                events.push(currentEvent);
+            }
         }
 
-        //Récupération de la localisation
-        var blocks= data.split("SEQUENCE");
-        var location= [];
-        for (i = 0, len = blocks.length; i < len-1; i++) {
-            var splited= blocks[i].split("LOCATION:");
-            location.push(splited[1]);
-        }
-        /*console.log("Description : "+description[0]);
-        console.log("Date de début : "+beginDate[0]);
-        console.log("Date de fin : "+endDate[0]);
-        console.log("Localisation : "+location[0]);*/
-
-        var events = {description : description,
-            beginDate : beginDate,
-            endDate : endDate,
-            location : location};
-
-        res.json(events);
+        // send result
+        res.json (events);
     });
 
 });
