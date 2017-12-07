c8533336262a56eccb797b7a34bc50a1d504a141
HMFouad <HMFouad@Yahoo.FR>
2017-12-07 11:49:59 +0100

    Click submitSignUpForm qui marche, manque de la requette d'insertion dans la bd

--- hbcc-app/src/app/components/home/home.component.ts
+++ hbcc-app/src/app/components/home/home.component.ts
@@ -10,49 +10,61 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
-    private signUpForm;
+    private signUpForm: FormGroup;
 
     public constructor (private httpClient: HttpClient,
       private router: Router) {}
 
     public ngOnInit (): void {
       this.signUpForm = new FormGroup({
-        'email': new FormControl('', [Validators.required, Validators.email]),
-        'password': new FormControl('', [Validators.required]),
+        firstName: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        // 'speciality': new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+        cpassword: new FormControl('', [Validators.required])
 
     });
     }
 
-    public submitSignUpForm () {
-              if (this.signUpForm.valid) {
-                  this.httpClient.post(
-                      this.signUpForm.value, {
-                          responseType: 'json'
-                      }).subscribe((response) => { // success
-                      console.log (response);
-                  }, (error) => { // error
-                      console.log (error);
-                  });
-              }
-          }
-public get firstNameFormControl () {
+public get firstName () {
   return this.signUpForm.get('firstName');
 }
 
-public get lastNameFormControl () {
+public get lastName () {
   return this.signUpForm.get('lastName');
 }
 
-public get emailFormControl () {
-  return this.signUpForm.get('lastName');
+public get email () {
+  return this.signUpForm.get('email');
 }
 
-public get specialityFormControl () {
+public get speciality () {
   return this.signUpForm.get('speciality');
 }
 
-public get passwordFormControl () {
-  return this.signUpForm.get('lastName');
-}
+public get password () {
+  return this.signUpForm.get('password');
 }
 
+public get cpassword () {
+  return this.signUpForm.get('cpassword');
+}
+
+public submitSignUpForm () {
+  console.log ('Test0!!!!!!!!!!!!!!!!!');
+  // if (this.signUpForm.valid) {
+
+      this.httpClient.post(
+          this.signUpForm.value, {
+              responseType: 'json'
+          }).subscribe((response) => { // success
+            console.log ('Réponse!!!!!!!!!!!!!!!!!');
+            console.log (response);
+      }, (error) => { // error
+          console.log ('Erreur!!!!!!!!!!!!!!!!!');
+          // console.log (error);
+      });
+  // }
+}
+}
