d61aa3b36b558248cec1da0e225fc736209c43a8
Adrien Bounader <adrien.bounader@etu.u-bordeaux.fr>
2017-12-08 13:28:10 +0100

    Commit for merge

--- hbcc-app/server/api/services/events.js
+++ hbcc-app/server/api/services/events.js
@@ -5,27 +5,42 @@ const icalParser = require('ical');
 const Event = require('../mongoose/model/event.model');
 
 // get static file
-const FILE_PATH = "../Master2_4TGL901S_iCalendar.ics";
+const FILE_PATH = "Master2_4TGL901S_iCalendar.ics";
 
-
-// Get student events
+/**
+ * Get student events
+ * Request GET params should contain:
+ *  * beginDate (ISO Date)
+ *  * endDate (ISO Date)
+ */
 router.get('/events', (req, res, next) => {
     console.log ('Api service call: GET events');
+
     const ICS_TYPE_WANTED = 'VEVENT';
 
-    fs.readFile(FILE_PATH, {encoding: "utf8"}, (err, data) => {
-        if (err) return next(err);
+    fs.readFile(FILE_PATH, { encoding: "utf8" }, (err, data) => {
+        if (err) {
+            return next(err);
+        }
 
         const events = [];
 
         const parsedEvents = icalParser.parseICS(data);
+
+        const beginDate = new Date (req.params.beginDate);
+        const endDate = new Date (req.params.endDate);
+
+        // check if beginDate is valid
+        if (!(beginDate instanceof Date) ||
+            isNaN(beginDate.getTime())) {
+            res.status();
+        }
 
         for (const indexEvent in parsedEvents) {
             if (parsedEvents.hasOwnProperty(indexEvent) &&
                 parsedEvents[indexEvent].type === ICS_TYPE_WANTED) {
                 const currentEvent = new Event();
 
-                // TODO parse description ?
                 if (parsedEvents[indexEvent].description) {
                     currentEvent.title = parsedEvents[indexEvent].description;
                 }
--- hbcc-app/src/app/components/home/home.component.ts
+++ hbcc-app/src/app/components/home/home.component.ts
@@ -11,11 +11,22 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
-    private signUpForm;
+    private signUpForm: FormGroup;
 
     private specialities: any;
 
+<<<<<<< .merge_file_5pdp4G
     public constructor(private httpClient: HttpClient) {}
+=======
+    public ngOnInit (): void {
+      this.signUpForm = new FormGroup({
+        firstName: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        // 'speciality': new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+        cpassword: new FormControl('', [Validators.required])
+>>>>>>> .merge_file_qsFyXG
 
     public ngOnInit(): void {
         this.signUpForm = new FormGroup({
@@ -32,6 +43,7 @@ export class HomeComponent implements OnInit {
         });
     }
 
+<<<<<<< .merge_file_5pdp4G
 
     public submitSignUpForm () {
         if (this.signUpForm.valid) {
@@ -64,5 +76,46 @@ export class HomeComponent implements OnInit {
     public get passwordFormControl () {
       return this.signUpForm.get('lastName');
     }
+=======
+public get firstName () {
+  return this.signUpForm.get('firstName');
 }
 
+public get lastName () {
+  return this.signUpForm.get('lastName');
+}
+
+public get email () {
+  return this.signUpForm.get('email');
+}
+
+public get speciality () {
+  return this.signUpForm.get('speciality');
+}
+
+public get password () {
+  return this.signUpForm.get('password');
+}
+
+public get cpassword () {
+  return this.signUpForm.get('cpassword');
+>>>>>>> .merge_file_qsFyXG
+}
+
+public submitSignUpForm () {
+  console.log ('Test0!!!!!!!!!!!!!!!!!');
+  // if (this.signUpForm.valid) {
+
+      this.httpClient.post(
+          this.signUpForm.value, {
+              responseType: 'json'
+          }).subscribe((response) => { // success
+            console.log ('Réponse!!!!!!!!!!!!!!!!!');
+            console.log (response);
+      }, (error) => { // error
+          console.log ('Erreur!!!!!!!!!!!!!!!!!');
+          // console.log (error);
+      });
+  // }
+}
+}
