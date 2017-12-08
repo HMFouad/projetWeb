655b2bd735b169d8348dff5cce735d31112d402a
HMFouad <HMFouad@Yahoo.FR>
2017-12-08 16:38:01 +0100

    Version inscription qui marche avec le mot de passe crypté

--- hbcc-app/server/api/services/users.js
+++ hbcc-app/server/api/services/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const checkAuth = require ('../check-auth');
 const User = require('../mongoose/model/user.model');
+const Speciality = require('../mongoose/model/speciality.model');
+const Encrypt = require('./encrypt');
 
 // Get users
 router.get('/users/:id', (req, res) => {
@@ -24,43 +26,29 @@ router.get('/users/:id', (req, res) => {
 });
 
 // Registration service
-router.post('/users', (req, res) => {console.log (req.body)
-    console.log('11111111111111111111')
-    res.json("Message!!!!!!!!!!!!")
-        firstName=req.body.firstName;
-        lastName=req.body.lastName;
-        password=req.body.password;
-        //rpassword=req.body.rpassword;
-        email=req.body.email;
-        //speciality=req.body.speciality;
+router.post('/users', (req, res) => {
+    Speciality.findOne({ name: req.body.speciality }, (speciality_err, spec) => {
+        if (speciality_err) { 
+            console.log("Speciality Error : " + speciality_err);
+        } else {
+                const user = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    password: Encrypt.encrypt(req.body.password),
+                    email: req.body.email,
+                    speciality: spec._id
+                });
 
-        const user = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: req.body.password,
-            //rpassword=req.body.rpassword;
-            email: req.body.email,
-            speciality: req.body.speciality
-        });
-
-        user.save((err, results) => {
-            if (err) {
-                console.log(err);
-                } else {
-                 console.log(results);
+                user.save(function(user_err, results) {
+                    if (user_err) {
+                        console.log("User Error: " + user_err);
+                        } else {
+                        console.log("Resultat: " + results);
+                    }
+                });
             }
-        });
-        //User.insert
-        /*
-    //mongoose.users.insert( {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,        
-        speciality: "Master 2 Génie Logiciel",
-        password: password,
-     } );*/
 
     });
-
+});
 
 module.exports = router;
