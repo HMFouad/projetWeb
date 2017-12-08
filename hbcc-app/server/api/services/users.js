bb6ba47f3fe68319999995745e2ff0c77556f6db
HMFouad <HMFouad@Yahoo.FR>
2017-12-08 14:40:33 +0100

    Merge

--- hbcc-app/server/api/services/users.js
+++ hbcc-app/server/api/services/users.js
@@ -24,22 +24,24 @@ router.get('/', (req, res) => {
 
 // Registration service
 router.post('/', (req, res) => {
-    connection((db) => {
-        res.json({ success: true });
-
+    console.log('11111111111111111111')
+    res.json("Message!!!!!!!!!!!!")
         firstName=req.body.firstName;
         lastName=req.body.lastName;
         password=req.body.password;
-        rpassword=req.body.rpassword;
+        //rpassword=req.body.rpassword;
         email=req.body.email;
         //speciality=req.body.speciality;
 
     mongoose.users.insert( {
         firstName: firstName,
         lastName: lastName,
-        password: password, email: email, speciality: "Master 2 Génie Logiciel" } );
+        email: email,        
+        speciality: "Master 2 Génie Logiciel",
+        password: password,
+     } );
 
     });
-});
+
 
 module.exports = router;
