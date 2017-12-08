00e197b271e11557d7af74d2ac16c0f698db6f58
Adrien Bounader <adrien.bounader@etu.u-bordeaux.fr>
2017-12-08 13:34:17 +0100

    Commit for merge

--- hbcc-app/src/app/components/home/home.component.ts
+++ hbcc-app/src/app/components/home/home.component.ts
@@ -15,20 +15,9 @@ export class HomeComponent implements OnInit {
 
     private specialities: any;
 
-<<<<<<< .merge_file_5pdp4G
     public constructor(private httpClient: HttpClient) {}
-=======
-    public ngOnInit (): void {
-      this.signUpForm = new FormGroup({
-        firstName: new FormControl('', [Validators.required]),
-        lastName: new FormControl('', [Validators.required]),
-        email: new FormControl('', [Validators.required, Validators.email]),
-        // 'speciality': new FormControl('', [Validators.required]),
-        password: new FormControl('', [Validators.required]),
-        cpassword: new FormControl('', [Validators.required])
->>>>>>> .merge_file_qsFyXG
 
-    public ngOnInit(): void {
+    public ngOnInit (): void {
         this.signUpForm = new FormGroup({
             'firstName': new FormControl('', [Validators.required]),
             'lastName': new FormControl('', [Validators.required]),
@@ -43,20 +32,6 @@ export class HomeComponent implements OnInit {
         });
     }
 
-<<<<<<< .merge_file_5pdp4G
-
-    public submitSignUpForm () {
-        if (this.signUpForm.valid) {
-            this.httpClient.post(
-                this.signUpForm.value, {
-                    responseType: 'json'
-                }).subscribe((response) => { // success
-                console.log(response);
-            }, (error) => { // error
-                console.log(error);
-            });
-        }
-    }
     public get firstNameFormControl () {
       return this.signUpForm.get('firstName');
     }
@@ -76,46 +51,21 @@ export class HomeComponent implements OnInit {
     public get passwordFormControl () {
       return this.signUpForm.get('lastName');
     }
-=======
-public get firstName () {
-  return this.signUpForm.get('firstName');
-}
 
-public get lastName () {
-  return this.signUpForm.get('lastName');
-}
+    public submitSignUpForm () {
+      console.log ('Test0!!!!!!!!!!!!!!!!!');
+      // if (this.signUpForm.valid) {
 
-public get email () {
-  return this.signUpForm.get('email');
-}
-
-public get speciality () {
-  return this.signUpForm.get('speciality');
-}
-
-public get password () {
-  return this.signUpForm.get('password');
-}
-
-public get cpassword () {
-  return this.signUpForm.get('cpassword');
->>>>>>> .merge_file_qsFyXG
-}
-
-public submitSignUpForm () {
-  console.log ('Test0!!!!!!!!!!!!!!!!!');
-  // if (this.signUpForm.valid) {
-
-      this.httpClient.post(
-          this.signUpForm.value, {
-              responseType: 'json'
-          }).subscribe((response) => { // success
-            console.log ('Réponse!!!!!!!!!!!!!!!!!');
-            console.log (response);
-      }, (error) => { // error
-          console.log ('Erreur!!!!!!!!!!!!!!!!!');
-          // console.log (error);
-      });
-  // }
-}
+          this.httpClient.post(
+              this.signUpForm.value, {
+                  responseType: 'json'
+              }).subscribe((response) => { // success
+                console.log ('Réponse!!!!!!!!!!!!!!!!!');
+                console.log (response);
+          }, (error) => { // error
+              console.log ('Erreur!!!!!!!!!!!!!!!!!');
+              // console.log (error);
+          });
+      // }
+    }
 }
