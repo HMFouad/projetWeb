2ba55cf5a063b09eade674336ee9af7f199dbb8f
Adrien Bounader <adrien.bounader@etu.u-bordeaux.fr>
2017-12-15 20:36:58 +0100

    commentaire

--- hbcc-app/server/server.js
+++ hbcc-app/server/server.js
@@ -1,14 +1,18 @@
 const http = require('http');
+
 const constants = require('./constants');
 const router = require('./router');
 
-const server = http.createServer(router);
 const dbConnection = require('./mongoose/db-connection').connect;
 const dbDisconnect = require('./mongoose/db-connection').disconnect;
 const dbConfig = require('./mongoose/db-config').default;
 
 const port = constants.HOST_PORT;
 
+// load router
+const server = http.createServer(router);
+
+// launch server
 server.listen(port, () => {
     console.log(`Running on localhost:${port}`);
     dbConnection(dbConfig)
@@ -16,6 +20,7 @@ server.listen(port, () => {
               () => console.log(`Database connection failed`));
 });
 
+// disconnect on ^C
 process.on('SIGINT', () => {
     dbDisconnect();
 });
